4f1a86ec604eb0a3c9f824bdc4e1b11d57e210a6
André Pereira <andrepereirabe@outlook.com>
2025-06-26 20:08:57 +0200

    feat(core): Make config file path come from a app argument.

--- internal/main.go
+++ internal/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"titan-lift/internal/config"
+	"titan-lift/internal/server_error"
+)
 
 func main() {
-	fmt.Println("Titan Lift")
+	serverConfig, err := getConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(serverConfig)
+}
+
+func getConfig() (*config.ServerConfig, error) {
+	if len(os.Args) < 2 {
+		return nil, server_error.New("INIT", "missing configuration file path as 1st argument")
+	}
+	file, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		return nil, server_error.Wrap("INIT", "error when reading config file", err)
+	}
+	return config.GetServerConfig(file)
 }
