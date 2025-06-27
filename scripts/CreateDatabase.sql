2c5311e851d143e32e8277eefbae827088b1e27f
André Pereira <andrepereirabe@outlook.com>
2025-06-27 07:41:35 +0200

    feat(core): Scripts for database creation.

---
+++ scripts/CreateDatabase.sql
@@ -0,0 +1,20 @@
+-- Creates a database and a user.
+-- Parameters:
+--   database_name: Name of the database to create
+--   username: Database username to create
+--   password: Password for the user
+
+
+CREATE DATABASE :database_name;
+
+CREATE USER :user_name WITH PASSWORD :password;
+GRANT CONNECT ON DATABASE :database_name TO :user_name;
+
+
+\c :database_name
+GRANT USAGE, CREATE ON SCHEMA public TO :user_name;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO :user_name;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO :user_name;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :user_name;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO :user_name;
